9dc7e1b2858371ac109f5d33a33ccab820f6aea8
PoleTT <poltt@mail.ru>
2018-12-29 22:18:24 +0300

    Set email utility

---
+++ src/main/java/dev/rozhkova/ibank/configuration/EmailConfiguration.java
@@ -0,0 +1,22 @@
+package dev.rozhkova.ibank.configuration;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class EmailConfiguration {
+
+    @Value("${email.util.host}")
+    private String host;
+
+    @Value("${email.util.port}")
+    private String port;
+
+    @Value("${email.util.user}")
+    private String user;
+
+    @Value("${email.util.pass}")
+    private String pass;
+}
--- src/main/java/dev/rozhkova/ibank/controller/BankAccountController.java
+++ src/main/java/dev/rozhkova/ibank/controller/BankAccountController.java
@@ -1,14 +1,19 @@
 package dev.rozhkova.ibank.controller;
 
+import dev.rozhkova.ibank.configuration.EmailConfiguration;
 import dev.rozhkova.ibank.dto.BankAccountDto;
+import dev.rozhkova.ibank.dto.RequestDto;
 import dev.rozhkova.ibank.exception.UserException;
 import dev.rozhkova.ibank.service.BankAccountService;
+import dev.rozhkova.ibank.utils.EmailUtility;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import java.util.List;
+import java.util.UUID;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -17,6 +22,7 @@ import static org.springframework.http.HttpStatus.OK;
 /*@RequestMapping("/bank_account")*/
 public class BankAccountController {
     private final BankAccountService bankAccountService;
+    private final EmailConfiguration emailConfiguration;
 
     @GetMapping("/bank_account/list")
     public ResponseEntity getAllBankAccount() {
@@ -43,9 +49,29 @@ public class BankAccountController {
         }
     }
 
+    @PostMapping("/bank_account/create")
+    public ResponseEntity unlockBankAccount(@RequestBody final RequestDto requestDto) {
+        String generatedValue = UUID.randomUUID().toString().replaceAll("-", "");
+        String recipient = requestDto.getEmail();
+        String msg = "This is your confirmation code: <a href=\"http://localhost:8080/myapp/registration?account="
+                + requestDto.getAccountNumber() + "&key=" + generatedValue + "\">activate</a>";
+        String subject = "Confirmation from MySite";
+        String host = emailConfiguration.getHost();
+        String port = emailConfiguration.getPort();
+        String fromEmail = emailConfiguration.getUser();
+        String pass = emailConfiguration.getPass();
+        try {
+            EmailUtility.sendEmailWithAttachment(host, port, fromEmail, pass, recipient, subject, msg, null);
+            return new ResponseEntity<>("New unlocking request created", HttpStatus.CREATED);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Something wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @DeleteMapping("/bank_account/remove")
     public ResponseEntity removeBankAccount(@RequestBody final BankAccountDto bankAccountDto) {
-        try{
+        try {
             bankAccountService.removeBankAccount(bankAccountDto);
             return new ResponseEntity<>("Bank account removed", OK);
         } catch (UserException ex) {
